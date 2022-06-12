748a42de60c5d531b477717fa66a1f2e2665b896
Sourav Pandya <pandyasourav38@gmail.com>
2022-06-12 19:40:24 -0400

    Project & client queries

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+require('dotenv').config();
+const { graphqlHTTP } = require('express-graphql')
+const schema = require('./schema/schema')
+const port = process.env.PORT || 5000;
+const app = express();
+const colors = require('colors')
+const db = require('./config/db');
+const connectDB = require('./config/db');
+
+//connect database
+connectDB();
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+
+}))
+
+app.listen(port, () => {
+    console.log(`Listening on server: ${port}`)
+})
